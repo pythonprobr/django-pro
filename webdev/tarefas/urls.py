773af0dac43093410a64e1b40cf8af4591ef34db
renzon <renzon@gmail.com>
2020-06-29 15:32:20 -0300

    Implementada lista de tarefas pendentes
    close #5
    close #8
    close #6

--- webdev/tarefas/urls.py
+++ webdev/tarefas/urls.py
@@ -5,5 +5,6 @@ from webdev.tarefas import views
 app_name='tarefas'
 
 urlpatterns=[
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<int:tarefa_id>', views.detalhe, name='detalhe'),
 ]
--- webdev/tarefas/views.py
+++ webdev/tarefas/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 
-from webdev.tarefas.forms import TarefaNovaForm
+from webdev.tarefas.forms import TarefaNovaForm, TarefaForm
 from webdev.tarefas.models import Tarefa
 
 
@@ -16,7 +16,28 @@ def home(request):
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
             tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
-            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes},
-                          status=400)
+            tarefas_feitas = Tarefa.objects.filter(feita=True).all()
+            return render(
+                request, 'tarefas/home.html',
+                {
+                    'form': form,
+                    'tarefas_pendentes': tarefas_pendentes,
+                    'tarefas_feitas': tarefas_feitas,
+                },
+                status=400)
     tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
-    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
+    tarefas_feitas = Tarefa.objects.filter(feita=True).all()
+    return render(request, 'tarefas/home.html',
+                  {
+                      'tarefas_pendentes': tarefas_pendentes,
+                      'tarefas_feitas': tarefas_feitas,
+                  }
+                  )
+
+
+def detalhe(request, tarefa_id):
+    tarefa = Tarefa.objects.get(id=tarefa_id)
+    form = TarefaForm(request.POST, instance=tarefa)
+    if form.is_valid():
+        form.save()
+    return HttpResponseRedirect(reverse('tarefas:home'))
